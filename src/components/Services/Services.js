fb996bd76766c210324960de8a63399c0ef39ac4
Felipe-alv-es <brago.sama@gmail.com>
2023-07-21 02:41:55 -0300

    chore(Service): finished service Slider

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,70 +1,51 @@
-import { Box, Typography } from "@mui/material";
-import React, { useEffect, useRef } from "react";
+import { Box, IconButton, Typography } from "@mui/material";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import chevron from "../../assets/icons/chevron.png";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+import serviceSlider from "../../assets/utils/serviceSlider.json";
+import {
+  getIconButtonStyle,
+  getTextBoxStyle,
+  getTitleStyle,
+  getTextStyle,
+  getContainerStyle,
+} from "./Services.styles.ts";
 
 const Services = () => {
-  const servicesOptions = [
-    {
-      id: 1,
-      title: "Meus Cartões",
-      text: "Gerencie suas opções \n de pagamento desfrutando de \n uma experiência \n personalizada para você ",
-    },
-    {
-      id: 2,
-      title: "Fatura Digital",
-      text: "Gerencie suas opções \n de pagamento desfrutando de \n uma experiência \n personalizada para você ",
-    },
-    {
-      id: 3,
-      title: "Atendimento",
-      text: "Gerencie suas opções \n de pagamento desfrutando de \n uma experiência \n personalizada para você ",
-    },
-    {
-      id: 4,
-      title: "Emprestimos",
-      text: "Gerencie suas opções \n de pagamento desfrutando de \n uma experiência \n personalizada para você ",
-    },
-  ];
+  const [swiper, setSwiper] = useState(null);
+  const swipeNextPage = () => swiper.slideTo(+1);
+  const swipePreviousPage = () => swiper.slideTo(-1);
 
   const params = {
-    navigation: true,
-    pagination: { clickable: true },
     slidesPerView: 3,
+    spaceBetween: "36px",
+    onSwiper: { setSwiper },
+    style: {
+      margin: "0 36px 0 36px",
+      background: "#f3f3f3",
+    },
   };
 
   return (
-    <Box margin={"16px 0 16px 0"}>
+    <Box sx={getContainerStyle}>
+      <IconButton onClick={swipePreviousPage} sx={getIconButtonStyle}>
+        <BsChevronLeft />
+      </IconButton>
       <Swiper {...params}>
-        {servicesOptions.map((item) => (
+        {serviceSlider.map((item) => (
           <SwiperSlide key={item.id}>
-            <Box
-              sx={{
-                padding: "32px",
-                borderWidth: "1px",
-                borderStyle: "solid",
-                borderRadius: "8px",
-                marginLeft: "8px",
-                marginRight: "8px",
-              }}
-            >
-              <Typography
-                variant="subtitle1"
-                fontWeight={"bold"}
-                color={"#5271FF"}
-                sx={{ whiteSpace: "pre-line" }}
-              >
-                {item.title}
-              </Typography>
+            <Box sx={getTextBoxStyle}>
+              <Typography sx={getTitleStyle}>{item.title}</Typography>
               <Box sx={{ m: 2 }} />
-              <Typography variant="body1" sx={{ whiteSpace: "pre-line" }}>
-                {item.text}
-              </Typography>
+              <Typography sx={getTextStyle}>{item.text}</Typography>
               <Box sx={{ m: 3 }} />
             </Box>
           </SwiperSlide>
         ))}
       </Swiper>
+      <IconButton onClick={swipeNextPage} sx={getIconButtonStyle}>
+        <BsChevronRight />
+      </IconButton>
     </Box>
   );
 };
